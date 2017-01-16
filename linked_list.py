43fbd8bae239642782b7eecf21a63549f4bbd785
shehryarkh <skhan8525@gmail.com>
2017-01-16 13:32:04 -0500

    linkedList

---
+++ linked_list.py
@@ -0,0 +1,14 @@
+__author__ = 'shehryar'
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    
