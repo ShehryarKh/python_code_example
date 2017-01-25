5d02ab1f9567b49a0e32d3d8927f7f0dbb8cb97b
shehryarkh <skhan8525@gmail.com>
2017-01-25 14:52:09 -0500

    linked_list, added search, seperated classes, added remove

--- linked_list.py
+++ linked_list.py
@@ -4,11 +4,40 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
+        self.prev = None
 
-    def getData(self):
-        return self.data
 
-    def getNext(self):
-        return self.next
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    
+    def add(self, data):
+        node = Node(data)
+
+        if self.head == None:
+            self.head = node
+
+        else:
+            node.next = self.head
+            node.next.prev = node
+            self.head = node
+
+
+
+    def search(self, key):
+        node = self.head
+        if node != None:
+            while node.next != None:
+                if (node.data == key):
+                    return node
+                node = node.next
+            if (node.data == key):
+                return node
+
+        return None
+
+    def remove(self, key):
+        pre = key.prev
+        key.prev.next = key.next
+        key.prev = pre
+
